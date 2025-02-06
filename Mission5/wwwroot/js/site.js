4bc1704916db8fab7e31f4cb21d8e31e0e3bb127
Sarah Walker <sarahsweetpie6@gmail.com>
2025-02-06 16:43:10 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿//event listener for when the button is clicked
+$('#btnCalc').click(function () {
+
+    var hours = $('#hours').val();
+
+    //check to see that input is less than 6 hours
+    if (hours > 6) {
+        alert('Please enter a number equal to or less than 6');
+    }
+    //check to see if input is positive
+    else if (hours <= 0) {
+        alert('Please enter a positive number');
+    }
+    else {
+        var rate = $('#charge').val();
+        var totalPrice = hours * rate;
+        //replace the text in price header with the total calculated price
+        $('#totalPrice').text(totalPrice);
+    }
+})
